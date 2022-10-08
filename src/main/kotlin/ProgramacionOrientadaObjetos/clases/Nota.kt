4ae54c381e4f006b242b03e6006a06f532fa48ed
Rey Mtz <reyjazzmtz@gmail.com>
2022-10-08 02:34:37 -0500

    [MT1O1-117] - Section 6 “Programación orientada a objetos (POO)" Video (25)
    - I created a package named ProgramacionOrientadaObjectos and made a video activity 25
    - inside this package create another package named clases to have the code more organized

---
+++ src/main/kotlin/ProgramacionOrientadaObjetos/clases/Nota.kt
@@ -0,0 +1,9 @@
+package ProgramacionOrientadaObjetos.clases
+
+import java.util.*
+
+class Nota {
+    var nombre: String = ""
+    var contenido: String = ""
+    var fechaCreacion: Date = Date()
+}
